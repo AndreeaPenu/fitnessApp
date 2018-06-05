70b63c4fc3e5b4f98f6ea1b8d5577ca12d414847
AndreeaPenu <andreea_penu@hotmail.Com>
2018-06-05 18:23:36 +0200

    packages for calendar

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,9 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import 'vue-event-calendar/dist/style.css' //^1.1.10, CSS has been extracted as one file, so you can easily update it.
+import vueEventCalendar from 'vue-event-calendar'
+Vue.use(vueEventCalendar, {locale: 'en'}) //locale can be 'zh' , 'en' , 'es', 'pt-br', 'ja', 'ko', 'fr', 'it', 'ru', 'de', 'vi', 'ua'
 
 require('./bootstrap');
 require('./video');
--- resources/assets/js/components/Plans.vue
+++ resources/assets/js/components/Plans.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-        <h2>Plans</h2>
-        
-    </div>
-   
+  <vue-event-calendar :events="demoEvents"></vue-event-calendar>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                
-            }
-        }
+export default {
+  data () {
+    return {
+      demoEvents: [{
+        date: '2016/11/12', // Required
+        title: 'Foo' // Required
+      }, {
+        date: '2016/12/15',
+        title: 'Bar',
+        desc: 'description',
+        customClass: 'disabled highlight' // Custom classes to an calendar cell
+      }]
     }
+  }
+}
 </script>
